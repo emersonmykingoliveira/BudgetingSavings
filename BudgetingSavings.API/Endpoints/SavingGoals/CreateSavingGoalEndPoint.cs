30df8669e8cbb4bbd898b81400595f07fe6f3b3c
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2026-04-09 21:54:48 +0200

    Adding shared project

--- BudgetingSavings.API/Endpoints/SavingGoals/CreateSavingGoalEndPoint.cs
+++ BudgetingSavings.API/Endpoints/SavingGoals/CreateSavingGoalEndPoint.cs
@@ -1,6 +1,15 @@
-﻿namespace BudgetingSavings.API.Endpoints.SavingGoals
+﻿using BudgetingSavings.API.Features;
+
+namespace BudgetingSavings.API.Endpoints.SavingGoals
 {
-    public class CreateSavingGoalEndPoint
+    public class CreateSavingGoalEndPoint : IEndpointDiscovery
     {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/createsavinggoal", async (HttpContext context, CreateSavingGoalRequest request) =>
+            {
+
+            }).RequireAuthorization("documentPolicy");
+        }
     }
 }
---
+++ BudgetingSavings.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetingSavings.Shared
+{
+    public class Class1
+    {
+
+    }
+}
