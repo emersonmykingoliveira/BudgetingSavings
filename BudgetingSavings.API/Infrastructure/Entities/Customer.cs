6fdee00c719f966da9e56c95a5c0acb1bf7eb21f
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2026-04-10 21:32:45 +0200

    Adding customer as entity

---
+++ BudgetingSavings.API/Infrastructure/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetingSavings.API.Infrastructure.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+    }
+}
