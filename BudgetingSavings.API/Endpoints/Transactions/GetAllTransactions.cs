1c52edbec01d824c44e7fab2b17dd864e2f70e3d
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2026-04-10 08:36:18 +0200

    Adding GetAllTransactionsEndPoint

---
+++ BudgetingSavings.API/Endpoints/Transactions/GetAllTransactions.cs
@@ -0,0 +1,15 @@
+﻿using BudgetingSavings.API.Features;
+
+namespace BudgetingSavings.API.Endpoints.Transactions
+{
+    public class GetAllTransactions : IEndpointDiscovery
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/transactions", async (HttpContext context) =>
+            {
+
+            });
+        }
+    }
+}
---
+++ BudgetingSavings.API/Endpoints/Transactions/GetTransactionByIdEndPoint.cs
@@ -0,0 +1,15 @@
+﻿using BudgetingSavings.API.Features;
+
+namespace BudgetingSavings.API.Endpoints.Transactions
+{
+    public class GetTransactionByIdEndPoint : IEndpointDiscovery
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/transactions/{transactionId}", async (HttpContext context, int transactionId) =>
+            {
+
+            });
+        }
+    }
+}
