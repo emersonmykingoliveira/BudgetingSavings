c54cd150a22d717f2c85fff2bb26fb6f835081d2
Emerson de Oliveira <Emerson.deOliveira@vitecsoftware.com>
2026-04-11 21:07:34 +0200

    Adding Budget entity

---
+++ BudgetingSavings.API/Infrastructure/Entities/Budget.cs
@@ -0,0 +1,14 @@
+﻿using BudgetingSavings.Shared.Models;
+using System.ComponentModel;
+
+namespace BudgetingSavings.API.Infrastructure.Entities
+{
+    public class Budget
+    {
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal LimitAmount { get; set; }
+        public CurrencyType Currency { get; set; }
+    }
+}
